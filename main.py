0926bac3de649a6d2e218f3d8f00c082a3870c9a
Sacrezar <thomas.soulas@viacesi.fr>
2020-09-13 21:50:56 +0200

    retrieve games from Steam API + working on multi threading

--- main.py
+++ main.py
@@ -1,34 +1,35 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from threading import Thread
-import concurrent.futures
 import random
+import	json
+import requests
 
 games = []
 reviews = []
 rated_games = []
 not_rated_games = []
 
-def get_games(url):
-    driver = webdriver.Firefox(executable_path=r'.\geckodriver.exe')
-    driver.get(url)
-    elem = driver.find_elements_by_class_name("gameListRow")
-    for i in elem:
+def get_games(account_id, api_key):
+    call = f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={api_key}&steamid={account_id}&include_appinfo=1&include_played_free_games=1&format=json"
+    response = requests.get(call)
+    games_info = response.json()["response"]["games"]
+    for i in games_info:
         games.append({
-                    "name":i.text.split("\n")[0], 
-                    "id": i.get_attribute('id').split("_")[1],
-                    "playtime":None,
+                    "name":i["name"], 
+                    "appid": i["appid"],
+                    "playtime":i["playtime_forever"],
                     "review_id":None
                     })
-    driver.close()
     return games
 
-def get_reviews(url):    
+def get_reviews(url,pages):    
     driver = webdriver.Firefox(executable_path=r'.\geckodriver.exe')
     driver.get(url)
     end_loop = False
+    nbr_loop = 0
 
-    while not end_loop:
+    while not end_loop or nbr_loop<=pages:
+        nbr_loop+=1
         elem = driver.find_elements_by_class_name("leftcol")
         x = 0
         for i in elem:
@@ -54,20 +55,41 @@ def get_reviews(url):
     driver.close()
     return reviews
 
-# Change URL there
-url = input("COPY PAST YOUR STEAM PROFILE URL (eg: https://steamcommunity.com/id/sacrezar/)\n")
+with open('user_info.json') as f:
+    info = json.load(f)
+
+api_steam_key = info["API_Steam_key"]
+steam_id = info["Steam_info"]["ID"]
+vanity_url = info["Steam_info"]["vanityURL"]
+url = "https://steamcommunity.com/id/" + vanity_url
 url_games = url + "games?tab=all"
 url_reviews = url + "/recommended/"
 
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    th1 = executor.submit(get_games, url_games)
-    th2 = executor.submit(get_reviews, url_reviews)
-    games = th1.result()
-    reviews = th2.result()
+# Retrieve number of pages
+driver = webdriver.Firefox(executable_path=r'.\geckodriver.exe')
+driver.get(url_reviews)
+elem = driver.find_elements_by_class_name("pagelink")
+nbr_pages = int(elem[2].text)-1
+driver.close() 
+
+# nbr_thread = 5
+# if nbr_pages>nbr_thread:
+#     nbr_pages_for_each_thread = int(nbr_pages / nbr_thread)
+#     nbr_pages_for_last_thread = int(nbr_pages % nbr_thread)
+# else: 
+#     nbr_thread = nbr_pages
+#     nbr_pages_for_each_thread= 1
+#     nbr_pages_for_last_thread= 1
+
+# print(nbr_pages_for_each_thread, nbr_pages_for_last_thread)
+
+games = get_games(steam_id,api_steam_key)
+reviews = get_reviews(url_reviews, 0)
+
 
 for review in reviews:
     for game in games:
-        if review["game_id"] == game["id"]:
+        if review["game_id"] == game["appid"]:
             review["game_name"] = game["name"]
             game["review_id"] = review["id"]
             break
