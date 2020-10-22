d3a070cd34082998317091bd0988aefc734e7d95
Sacrezar <thomas.soulas@viacesi.fr>
2020-10-22 09:30:57 +0200

    flake8 linting

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
-import re
-import pprint
-import numpy as np 
-import json 
+# import re
+# import pprint
+import numpy as np
+import json
 import random
 
 from bs4 import BeautifulSoup
@@ -11,31 +11,35 @@ from userinfo import UserInfo
 
 NB_REVIEWS_PER_PAGE = 10
 
+
 def get_soup(url):
     page = requests.get(url)
     return BeautifulSoup(page.content, "html.parser")
 
+
 def get_nb_reviews(soup):
-    nb_reviews = soup.find('div', class_ = "giantNumber")
+    nb_reviews = soup.find('div', class_="giantNumber")
     return nb_reviews.get_text()
+
 
 def reviews(soup):
     all_data_reviews = []
-    reviews = soup.find_all('div', class_ = "review_box")
+    reviews = soup.find_all('div', class_="review_box")
     for review in reviews:
         data_review = ReviewHandler(review)
         all_data_reviews.append(
             {
-            "game_id": data_review.get_game_id(),
-            "recommendation": data_review.get_review_status(),
-            "appreciation": data_review.get_appreciation(),
-            "awards_info":data_review.get_awards(info=False),
-            "posted_date": data_review.get_date()["posted"],
-            "last_edit": data_review.get_date()["last_edit"],
-            "total_words": data_review.get_total_words()
+                "game_id": data_review.get_game_id(),
+                "recommendation": data_review.get_review_status(),
+                "appreciation": data_review.get_appreciation(),
+                "awards_info": data_review.get_awards(info=False),
+                "posted_date": data_review.get_date()["posted"],
+                "last_edit": data_review.get_date()["last_edit"],
+                "total_words": data_review.get_total_words()
             }
         )
     return all_data_reviews
+
 
 def get_urls(url, nb_reviews):
     urls = []
@@ -48,16 +52,19 @@ def get_urls(url, nb_reviews):
 
 def get_games(account_id, api_key):
     games = []
-    call = f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={api_key}&steamid={account_id}&include_appinfo=1&include_played_free_games=1&format=json"
+    call = "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/"\
+        f"?key={api_key}&steamid={account_id}"\
+        "&include_appinfo=1&include_played_free_games=1&format=json"
     response = requests.get(call)
     games_info = response.json()["response"]["games"]
     for i in games_info:
         games.append({
-                    "name":i["name"], 
+                    "name": i["name"],
                     "appid": i["appid"],
-                    "playtime":i["playtime_forever"]
+                    "playtime": i["playtime_forever"]
                     })
     return games
+
 
 def get_user_info():
     with open('user_info.json') as f:
@@ -70,7 +77,7 @@ def main():
     account_id = user_info.get_account_id()
     api_key = user_info.get_api_key()
 
-    url = "https://steamcommunity.com/profiles/" + account_id +"/recommended/"
+    url = "https://steamcommunity.com/profiles/" + account_id + "/recommended/"
     reviews_data = []
     soup = get_soup(url)
     nb_reviews = int(get_nb_reviews(soup))
@@ -80,37 +87,39 @@ def main():
         print(f"Fetching data from {url}")
         soup = get_soup(url)
         reviews_data += reviews(soup)
-    
+
     print("")
 
     games = get_games(account_id, api_key)
-    reviews_ = reviews_data.copy()
+    # reviews_ = reviews_data.copy()
     for game in games:
         for review in reviews_data:
             if review["game_id"] == game["appid"]:
                 del review["game_id"]
-                game["review_data"] = review 
+                game["review_data"] = review
                 reviews_data.remove(review)
-    
+
     print(f"There are {len(reviews_data)} reviews for unknown games (dlc?)")
 
     for review in reviews_data:
         appid = review["game_id"]
         del review["game_id"]
-        call = f"http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid={appid}&key={api_key}&steamid={account_id}"
+        call = "http://api.steampowered.com/ISteamUserStats/"\
+            "GetUserStatsForGame/v0002/"\
+            f"?appid={appid}&key={api_key}&steamid={account_id}"
         response = requests.get(call)
         if response:
             game_name = response.json()["playerstats"]["gameName"]
-        else: 
+        else:
             print(f"Can't find [appid: {appid}]")
             game_name = "Unknown"
         games.append({
-            "name":game_name,
-            "appid":appid,
-            "playtime":None,
+            "name": game_name,
+            "appid": appid,
+            "playtime": None,
             "review_data": review
         })
-    
+
     print("")
 
     not_reviewed_games = []
@@ -121,15 +130,17 @@ def main():
                 reviewed_games.append(game)
         except KeyError:
             not_reviewed_games.append(game)
-    print(f"You have \n\t{len(games)} games, \n\t{len(reviewed_games)} reviews, \n\t{len(not_reviewed_games)} games not reviewed.")
-    
+    print(
+        f"You have \n\t{len(games)} games, \n\t{len(reviewed_games)} reviews,"
+        f"\n\t{len(not_reviewed_games)} games not reviewed."
+    )
+
     print("")
 
-    print("10 random games not evaluated:") 
+    print("10 random games not evaluated:")
     games_to_review = random.sample(not_reviewed_games, 10)
     for game in games_to_review:
         print(game["name"])
-
 
 
 if __name__ == "__main__":
--- reviewhandler.py
+++ reviewhandler.py
@@ -19,7 +19,8 @@ def convert_date(date):
     ]
     if date == "":
         return None
-    date = date.replace(",","")
+
+    date = date.replace(",", "")
     date = date.split(" ")
     month = months.index(date[1]) + 1
     date_to_return = date[0] + "/" + str(month) + "/"
@@ -33,51 +34,58 @@ def convert_date(date):
 
 class ReviewHandler:
 
-
     def __init__(self, review):
         self.review = review
-
 
     def get_appreciation(self):
         nb_helpful = 0
         nb_funny = 0
-        appreciation = self.review.find('div', class_ = "header").get_text()
+        appreciation = self.review.find('div', class_="header").get_text()
 
         try:
-            nb_helpful = re.findall("\t([0-9]*?) (people|person) found this review helpful",appreciation)[0][0]
+            nb_helpful = re.findall(
+                "\t([0-9]*?) (people|person) found this review helpful",
+                appreciation
+                )[0][0]
         except IndexError:
             pass
         try:
-            nb_funny = re.findall("\t([0-9]*?) (people|person) found this review funny",appreciation)[0][0]
+            nb_funny = re.findall(
+                "\t([0-9]*?) (people|person) found this review funny",
+                appreciation)[0][0]
         except IndexError:
             pass
-        
+
         return {
             "nb_helpful": int(nb_helpful),
             "nb_funny": int(nb_funny)
         }
 
-
     def get_game_id(self):
-        title = self.review.find('div', class_ = "title")
+        title = self.review.find('div', class_="title")
         game_url = title.find(href=True)
         game_id = int(re.search("[0-9]+", game_url['href']).group())
         return game_id
-
 
     def get_awards(self, info=True):
         awards_info = []
         total_awards = 0
         total_points = 0
 
-        awards = self.review.find_all('div', class_ = "review_award")
+        awards = self.review.find_all('div', class_="review_award")
 
         for award in awards:
             data = award.get("data-tooltip-html")
-            
-            award_name = re.findall('<div class="reaction_award_name">(.*?)</div>', data)[0]
-            award_count = re.findall('<div class="reaction_award_count">(.*?)</div>', data)[0]
-            award_points = re.findall('<div class="reaction_award_points">(.*?)</div>', data)[0]
+
+            award_name = re.findall(
+                '<div class="reaction_award_name">(.*?)</div>', data
+                )[0]
+            award_count = re.findall(
+                '<div class="reaction_award_count">(.*?)</div>', data
+                )[0]
+            award_points = re.findall(
+                '<div class="reaction_award_points">(.*?)</div>', data
+                )[0]
             award_count = int(re.search("[0-9]+", award_count).group())
             award_points = int(re.search("[0-9]+", award_points).group())
 
@@ -102,9 +110,8 @@ class ReviewHandler:
 
         return all_awards_infos
 
-
     def get_date(self):
-        dates = self.review.find('div', class_ = "posted").get_text()
+        dates = self.review.find('div', class_="posted").get_text()
         posted = re.findall('Posted (.*?)[.]', dates)[0]
         try:
             edited = re.findall('Last edited (.*?)[.]', dates)[0]
@@ -112,15 +119,14 @@ class ReviewHandler:
             edited = ""
 
         return {
-            "posted":convert_date(posted),
+            "posted": convert_date(posted),
             "last_edit": convert_date(edited)
         }
-
 
     def get_total_words(self):
 
         to_split = r"[:|;|,|.\s|?|!|\s]"
-        content = self.review.find('div', class_ = "content")
+        content = self.review.find('div', class_="content")
         text = content.get_text()
 
         total_words = re.split(to_split, text)
@@ -128,10 +134,7 @@ class ReviewHandler:
 
         return len(total_words)
 
-
     def get_review_status(self):
-        title = self.review.find('div', class_ = "title")
+        title = self.review.find('div', class_="title")
         status = title.get_text()
         return status
-
-
